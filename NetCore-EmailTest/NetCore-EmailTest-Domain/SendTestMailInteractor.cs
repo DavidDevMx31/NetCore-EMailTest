7038531356f1e4bf97007d505fc48334d4d69374
David Castillo <davidc.mendoza31@gmail.com>
2020-07-16 15:39:52 -0500

    Validates the request content

--- NetCore-EmailTest/NetCore-EmailTest-Domain/SendTestMailInteractor.cs
+++ NetCore-EmailTest/NetCore-EmailTest-Domain/SendTestMailInteractor.cs
@@ -9,7 +9,62 @@ namespace NetCore_EmailTest_Domain
     {
         public void StartProcess(SendTestMailRequest request)
         {
-            throw new NotImplementedException();
+            var validationResult = ValidateRequest(request);
+
+            if (string.IsNullOrWhiteSpace(validationResult))
+            {
+
+            }
+            // Show errors to the user
+        }
+
+        private string ValidateRequest(SendTestMailRequest request)
+        {
+            StringBuilder errorsFound = new StringBuilder();
+
+            ValidateIssuerAccountRequiredFields(request, errorsFound);
+            ValidateReceiverAccountRequiredFields(request, errorsFound);
+            ValidateEmailContentRequiredFields(request, errorsFound);
+            
+            if (!int.TryParse(request.Port, out int port))
+                errorsFound.AppendLine("The port contain an integer number.");
+
+            return errorsFound.ToString();
+        }
+
+        private void ValidateIssuerAccountRequiredFields(SendTestMailRequest request, StringBuilder errorsFound)
+        {
+            if (CheckIfRequiredFieldIsEmpty(request.Server))
+                errorsFound.AppendLine("The SMTP server data is required.");
+
+            if (CheckIfRequiredFieldIsEmpty(request.Port))
+                errorsFound.AppendLine("The SMTP port is required.");
+
+            if (CheckIfRequiredFieldIsEmpty(request.Username))
+                errorsFound.AppendLine("The issuer email address is required.");
+
+            if (CheckIfRequiredFieldIsEmpty(request.Password))
+                errorsFound.AppendLine("The email password is required.");
+        }
+
+        private void ValidateReceiverAccountRequiredFields(SendTestMailRequest request, StringBuilder errorsFound)
+        {
+            if (CheckIfRequiredFieldIsEmpty(request.ToAddress))
+                errorsFound.AppendLine("The receiver email is required.");
+        }
+
+        private void ValidateEmailContentRequiredFields(SendTestMailRequest request, StringBuilder errorsFound)
+        {
+            if (CheckIfRequiredFieldIsEmpty(request.MailMessage))
+                errorsFound.AppendLine("The email message is required.");
+        }
+
+        private bool CheckIfRequiredFieldIsEmpty(string fieldData)
+        {
+            if (string.IsNullOrWhiteSpace(fieldData))
+                return true;
+
+            return false;
         }
 
     }
