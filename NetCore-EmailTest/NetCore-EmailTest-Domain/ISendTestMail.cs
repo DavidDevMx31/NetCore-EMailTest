f2c68e75d57d20a39be4d1c11e5fe9083ef9c939
David Castillo <davidc.mendoza31@gmail.com>
2020-07-16 14:02:13 -0500

    Adds ISendTestMail interface in domain layer

---
+++ NetCore-EmailTest/NetCore-EmailTest-Domain/ISendTestMail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore_EmailTest_Domain
+{
+    public interface ISendTestMail
+    {
+        void StartProcess();
+    }
+}
---
+++ NetCore-EmailTest/NetCore-EmailTest-Domain/SendTestMailInteractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore_EmailTest_Domain
+{
+    public class SendTestMailInteractor : ISendTestMail
+    {
+        public void StartProcess()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
