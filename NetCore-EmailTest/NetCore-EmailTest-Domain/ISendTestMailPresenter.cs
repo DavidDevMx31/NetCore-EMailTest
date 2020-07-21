a5c94f3917e2e968401f0315f9a24a64020edd59
David Castillo <davidc.mendoza31@gmail.com>
2020-07-21 15:28:35 -0500

    Add presenter implementation class, referencing domain layer

---
+++ NetCore-EmailTest/NetCore-EmailTest-Domain/ISendTestMailPresenter.cs
@@ -0,0 +1,12 @@
+﻿using NetCore_EmailTest_Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore_EmailTest_Domain
+{
+    public interface ISendTestMailPresenter
+    {
+        void PresentResult(SendTestMailResult testMailResult);
+    }
+}
---
+++ NetCore-EmailTest/NetCore-EmailTest-Presenter/SendTestMailPresenter.cs
@@ -0,0 +1,14 @@
+﻿using NetCore_EmailTest_Domain;
+using NetCore_EmailTest_Domain.Models;
+using System;
+
+namespace NetCore_EmailTest_Presenter
+{
+    public class SendTestMailPresenter : ISendTestMailPresenter
+    {
+        public void PresentResult(SendTestMailResult testMailResult)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
