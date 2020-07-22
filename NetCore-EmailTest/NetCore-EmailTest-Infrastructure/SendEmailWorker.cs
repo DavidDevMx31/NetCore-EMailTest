99389f5cce95a617e0dd405c6a8672dc0125a9f1
David Castillo <davidc.mendoza31@gmail.com>
2020-07-22 15:45:38 -0500

    Add Mailkit nuget package and reference domain layer

---
+++ NetCore-EmailTest/NetCore-EmailTest-Infrastructure/SendEmailWorker.cs
@@ -0,0 +1,51 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using NetCore_EmailTest_Domain.Models;
+using System;
+
+namespace NetCore_EmailTest_Infrastructure
+{
+    public class SendEmailWorker
+    {
+        public void SendEmail(TestMailModel testMail)
+        {
+            var email = CreateMail(testMail);
+            Send(email, testMail.SMTPConfiguration);
+        }
+
+        private MimeMessage CreateMail(TestMailModel model)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(model.From.UserName, model.From.Address));
+            email.To.Add(new MailboxAddress(model.To.UserName, model.To.Address));
+            email.Subject = model.Content.Subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = model.Content.Message };
+
+            return email;
+        }
+
+        private void Send(MimeMessage email, EmailConfiguration configuration)
+        {
+            using var client = new SmtpClient();
+
+            try
+            {
+                client.Connect(configuration.Server, configuration.Port, configuration.UseSSL);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(configuration.UserName, configuration.Password);
+
+                client.Send(email);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.InnerException);
+                throw;
+            }
+            //finally
+            //{
+            //    client.Disconnect(true);
+            //    client.Dispose();
+            //}
+        }
+    }
+}
